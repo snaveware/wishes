b995ea4b98bd17bee1ef3364d9c16502ab4cff81
snaveware <work.evans020@gmail.com>
2023-12-24 23:11:07 +0300

    added google analytics scripts to body

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -3,19 +3,19 @@ import { useEffect } from "react";
 
 export default function Analytics() {
   useEffect(() => {
-    const analyticsCode = `       <script async src="https://www.googletagmanager.com/gtag/js?id=G-CRGEYG5RG2"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-CRGEYG5RG2');
-        </script>`;
-    const analyticsDiv = document.createElement("div");
+    const script1 = document.createElement("script");
+    script1.src = "https://www.googletagmanager.com/gtag/js?id=G-CRGEYG5RG2";
+    script1.async = true;
+    document.body.appendChild(script1);
 
-    analyticsDiv.innerHTML = analyticsCode;
-
-    document.body.appendChild(analyticsDiv);
+    const script2 = document.createElement("script");
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-CRGEYG5RG2');
+    `;
+    document.body.appendChild(script2);
   }, []);
 
   return <></>;
